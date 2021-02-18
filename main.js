4d11059ba040a56e3f72c539348e221d45ab8d01
Marian Ovidiu Hutanu <h.marian914@gmail.com>
2021-02-18 17:47:52 +0100

    Aggiunta Nav-bar

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 var app = new Vue({
   el : '#root',
   data: {
-
+    navbar: [
+      {option: 'HOME', link: '#home'},
+      {option: 'SERVICE', link: '#service'},
+      {option: 'ABOUT', link: '#about'},
+      {option: 'PROJECTS', link: '#projects'},
+      {option: 'RESULTS', link: '#results'},
+      {option: 'GET IN TOUCH', link: '#get'},
+    ]
   },
   methods: {
 
